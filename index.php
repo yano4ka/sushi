f33b1ff595c9865966f1879a0c8f64aa962accb1
yano4ka <sinyavskaya@bigmir.net>
2013-05-26 10:07:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,10 @@
 <html>
 <head>
-<title>Stoumost_zakaza</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Суші</title>
 </head>
 <body>
-<H1> Замовлення суші </H1>
-<table border=2>
-<tr> <td>name</td><td>kilkist</td> <td>result</td> </tr>
-<?php
-$fp=fopen("P.txt","r");
-if ($fp)
-
-   {
-       while(!feof($fp))
-
-    {
-       $stroka=fscanf($fp,"%s\t %s\t %d\t %d\n");
-       list($name,$kilkist,$result)=$stroka;
-        //$kilkist=fscanf($fp,"%d\t");
-        //$result=fscanf($fp,"%d\n");
-echo "<TR><td>$name</td><td>$kilkist</td> <td>$result</td> </TR>";
-}
-fclose($fp);
-
-}
-?>
-</body>
-</html>
-<a href="calculator.html"> kalk</a>
+<a href="synyavska.htm"> Замовити</a><br>
+<a href="lip.php"> Продивиться замовлення </a>
 </body>
 </html>
